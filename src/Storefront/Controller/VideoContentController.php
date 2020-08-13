7495dd1430ab7eb3f58b18966239cb33eeb4e838
tinect <s.koenig@tinect.de>
2020-08-13 21:11:57 +0200

    add result type

--- src/Storefront/Controller/VideoContentController.php
+++ src/Storefront/Controller/VideoContentController.php
@@ -6,6 +6,7 @@ use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,7 +17,7 @@ class VideoContentController extends StorefrontController
     /**
      * @Route("/videocontent/load", name="frontend.videocontent.load", options={"seo"="false"}, methods={"GET"}, defaults={"XmlHttpRequest"=true})
      */
-    public function load(Request $request, SalesChannelContext $context)
+    public function load(Request $request, SalesChannelContext $context): Response
     {
         $url = $request->query->get('url', '');
         return $this->render('@FroshCmsVideoModals/storefront/videocontent/load.html.twig',
